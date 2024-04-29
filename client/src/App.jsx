7c8468dfade96379aa11ac2a93f73e2e78213f4e
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-04-29 23:52:47 +0500

    Login Page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
 
 function App() {
   return (
     <Routes>
-      <Route index element={ <IndexPage/> }  />
-      {/* <Route index element={ <IndexPage/> }  /> */}
-      
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
-  
   );
 }
 
