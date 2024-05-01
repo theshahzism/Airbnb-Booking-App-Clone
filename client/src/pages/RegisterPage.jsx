61a6f371c8f152f8f3479f658f4f37c8e559f850
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-05-01 11:40:38 +0500

    Updates

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = (ev) => {
+    ev.preventDefault();
+    axios.get('http://localhost:4000/test')
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-32">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-            <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500 ">
             Already a member?{" "}
