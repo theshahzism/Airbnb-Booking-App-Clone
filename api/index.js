4de6ab49b3b04bb4e3d9b776ca25ffcc5ec690d2
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-05-04 10:39:32 +0500

    Logout completed

--- api/index.js
+++ api/index.js
@@ -76,4 +76,10 @@ app.get("/profile", (req, res) => {
   }
 });
 
+app.post('/logout',(req,res)=>{
+  res.cookie('token','').json(true)
+})
+
+
+
 app.listen(4000);
--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,40 +1,63 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../UserContext'
-import { Link, Navigate, useParams } from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import { UserContext } from "../UserContext";
+import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const AccountPage = () => {
-  const {user,ready}=useContext(UserContext)
-
-  if (!ready){
-    return "Loading..."
+  const [redirect,setRedirect]=useState(null);
+  const { user, ready,setUser } = useContext(UserContext);
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
   }
 
-  if (ready && !user){
-    return <Navigate to={'/login'} />
+  if (!ready) {
+    return "Loading...";
   }
 
-  let {subpage}=useParams();
-  if(subpage===undefined){
-    subpage='profile';
+  if (ready && !user && !redirect) {
+    return <Navigate to={"/login"} />;
   }
 
-  function linkClasses (type=null){
-    let classes= 'py-2 px-6';
-    if(type===subpage){
-      classes+=' bg-primary text-white rounded-full'
+  async function logout() {
+    await axios.post("/logout");
+    setUser(null)
+    setRedirect('/')
+  }
+
+  function linkClasses(type = null) {
+    let classes = "py-2 px-6";
+    if (type === subpage) {
+      classes += " bg-primary text-white rounded-full";
     }
     return classes;
   }
 
-  return (
-    <div>
-      <nav className='w-full flex justify-center  mt-8 gap-2'>
-        <Link className={linkClasses('profile')} to={'/account'}>My profile</Link>
-        <Link className={linkClasses('bookings')} to={'/account/bookings'}>My bookings</Link>
-        <Link className={linkClasses('places')} to={'/account/places'}>My places</Link>
-      </nav>
-    </div>
-  )
+if (redirect){
+  return <Navigate to={redirect} />
 }
 
-export default AccountPage
+  return (
+    <div>
+      <nav className="w-full flex justify-center  mt-8 gap-2">
+        <Link className={linkClasses("profile")} to={"/account"}>
+          My profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          My bookings
+        </Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>
+          My places
+        </Link>
+      </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto ">
+          Logged in as {user.name} ({user.email}) <br />
+          <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AccountPage;
